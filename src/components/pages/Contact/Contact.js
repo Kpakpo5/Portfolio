1e022931d403a40040584e5898f9171e8b3ed156
Kpakpo5 <kpakpo.akue5@gmail.com>
2022-01-25 17:56:27 +0100

    feat(ContactSection): add emailjs package and sendMail function

--- src/components/pages/Contact/Contact.js
+++ src/components/pages/Contact/Contact.js
@@ -1,3 +1,4 @@
+import emailjs from '@emailjs/browser';
 import { SectionTitle } from "../../../globalStyles";
 import {
   ContactSection,
@@ -12,6 +13,21 @@ import {
 import Zoom from 'react-reveal/Zoom';
 
 const Contact = () => {
+
+  const sendMail = (e) => {
+    e.preventDefault();
+    emailjs.sendForm(
+      'service_frlfeyi',
+      'template_ro9lgea',
+      e.target,
+      'user_vIul8v33ok27Wx1Ydjx9g'
+    ).then((res) => {
+      console.log('SUCCESS!', res.status, res.text);
+   }, (err) => {
+      console.log('FAILED...', err);
+   });
+  }
+
   return (
     <>
       <ContactSection id="contact">
@@ -22,11 +38,11 @@ const Contact = () => {
           <Cv>Curriculum Vitae</Cv>
         </CvContainer>
         <FormContainer>
-          <ContactForm>
+          <ContactForm onSubmit={sendMail}>
             <label htmlFor="name">Votre nom</label>
             <ContactInput type="text" name="name" id="name"/>
-            <label htmlFor="e-mail">Votre e-mail</label>
-            <ContactInput type="email" name="email" id="email"/>
+            <label htmlFor="user_email">Votre e-mail</label>
+            <ContactInput type="email" name="user_email" id="user_email"/>
             <label htmlFor="message">Votre message</label>
             <ContactTextArea name="message"/>
             <SubmitButton>Envoyer</SubmitButton>
